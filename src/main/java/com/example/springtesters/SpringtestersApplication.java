eb9578cbd8ee37919d0caae8164d3211a9bd7c2a
Setsuna Yukiii <siang-meng@live.com>
2022-09-13 23:59:12 +0800

    Initial commit

---
+++ src/main/java/com/example/springtesters/SpringtestersApplication.java
@@ -0,0 +1,13 @@
+package com.example.springtesters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springtesters/SpringtestersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springtesters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringtestersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
